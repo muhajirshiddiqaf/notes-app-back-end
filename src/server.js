57510ae638ab240c31e74c61a11af285de444799
bagas31 <faruq.al.ms@gmail.com>
2021-08-12 06:29:37 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require('@hapi/hapi');
+const Routes = require('./router/notes.router')
+ 
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },  
+  });
+ 
+ 
+  server.route(Routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+ 
+init();
